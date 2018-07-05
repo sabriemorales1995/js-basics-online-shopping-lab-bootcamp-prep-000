1934818fe2b4a522db18ac4e0ff29544932805ad
Sabrie Morales <sabriemorales1995@gmail.com>
2018-07-05 17:09:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,8 +47,15 @@ function total() {
 }
 
 function removeFromCart(item) {
+  for(var i=0; i<cart.length; i++){
+    if(cart[i].itemName==item){
+      cart= [...cart.slice(0,i),...cart.slice(i+1)];
+      return cart;
+    }
+  }
+  return `That item is not in your cart.`;
   //if item is in cart remove and return updated cart
-  //if item not in cart, return 
+  //if item not in cart, return \\That item is not in your cart.
 }
 
 function placeOrder(cardNumber) {
